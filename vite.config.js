266e0baab93193e3cb8d7435b3947e4da923d3f8
CristianDev994 <cristian_davidvc03@hotmail.com>
2025-06-06 20:27:49 +0200

    Normalizo finales de línea y subo cambios

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+// Reemplaza 'cdvc-portfolio' por EXACTAMENTE el nombre de tu repositorio
+export default defineConfig({
+  base: '/cdvc-portfolio/',
+  // otras opciones de Vite que uses (p. ej. plugins, alias, etc.)
+});
